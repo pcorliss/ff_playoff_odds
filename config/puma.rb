0fc9bf8c0c4b64005b296a1428715128c51fa77b
Philip Corliss <pcorliss@50projects.com>
2024-03-21 14:59:49 -0500

    Dependabot Updates

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,11 @@
+require 'rack'
+
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
-rackup      DefaultRackup
 port        ENV['PORT']     || 9393
 environment ENV['RACK_ENV'] || 'development'
 
